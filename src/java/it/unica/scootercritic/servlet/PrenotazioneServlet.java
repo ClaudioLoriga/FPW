9efbde2cb2a0cfc70b38d4fc9a5299ec0febb0b9
Claudio Loriga <loriga.claudio.ca@gmail.com>
2021-12-06 08:58:39 -0800

    ordinamento 90 giorni fatto

--- src/java/it/unica/scootercritic/servlet/PrenotazioneServlet.java
+++ src/java/it/unica/scootercritic/servlet/PrenotazioneServlet.java
@@ -3,26 +3,69 @@ package it.unica.scootercritic.servlet;
 import java.io.IOException;
 import it.unica.scootercritic.model.SessioneDonazione;
 import it.unica.scootercritic.model.SessioneDonazioneFactory;
+import it.unica.scootercritic.model.Utente;
+import java.sql.Date;
+import java.time.LocalDate;
+import static java.time.temporal.ChronoUnit.DAYS;
+import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet(name = "PrenotazioneServlet", urlPatterns = {"/PrenotazioneServlet"})
 public class PrenotazioneServlet extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-                                         
-        List<SessioneDonazione> sessioni = SessioneDonazioneFactory.getInstance().getAllSessioni();
-        SessioneDonazione sessioneSelezionata = new SessioneDonazione();
-        request.setAttribute("listaSessioni", sessioni);
-        request.setAttribute("sessioneSelezionata", sessioneSelezionata);
-        request.getRequestDispatcher("nuova-prenotazione.jsp").forward(request, response);
 
-        String command = request.getParameter("offsetId");
+        HttpSession session = request.getSession(); // Crea una nuova sessione o recpera quella esistente
+        Utente utente_sessione = (Utente) session.getAttribute("utente");
+        String genericError = "Non sono presenti prenotazioni disponibili";
+
+        List<SessioneDonazione> sessioni = SessioneDonazioneFactory.getInstance().getAllSessioni();
+        List<SessioneDonazione> sessioniUtente = SessioneDonazioneFactory.getInstance().getAllSessioniUtente(utente_sessione);
+        List<SessioneDonazione> sessioniSelezionate = new ArrayList<>();
+
+        if (sessioni != null && sessioni.size() > 0) {
+
+            SessioneDonazione sessionePiuRecente = null;
+            for (SessioneDonazione sessione : sessioniUtente) {
+                if (sessionePiuRecente == null || sessionePiuRecente.getData_sessione().before(sessione.getData_sessione())) {
+                    sessionePiuRecente = sessione;
+                }
+            }
+
+            if (sessionePiuRecente == null) {
+                pubblicaLista(request, response, sessioni);
+            } else {
+
+                LocalDate localDateRecente = sessionePiuRecente.getData_sessione().toLocalDate();
+
+                for (SessioneDonazione sessione : sessioni) {
+                    LocalDate localDateSessione = sessione.getData_sessione().toLocalDate();
+                    if (sessione.getData_sessione().after(sessionePiuRecente.getData_sessione())) {
+                        long daysBetween = DAYS.between(localDateRecente, localDateSessione);
+                        if (daysBetween >= 90) {
+                            sessioniSelezionate.add(sessione);
+                        }
+                    }
+                }
+                if (sessioniSelezionate != null && sessioniSelezionate.size() > 0) {
+                    pubblicaLista(request, response, sessioniSelezionate);
+                } else {
+                    pubblicaErrore(request, response, genericError);
+
+                }
+            }
+        } else {
+            pubblicaErrore(request, response, genericError);
+        }
+
+        /*String command = request.getParameter("offsetId");
         if (command != null) {
             SessioneDonazione sessione = SessioneDonazioneFactory.getInstance().getSessione(command);
             request.setAttribute("sessione", sessione);
@@ -31,9 +74,7 @@ public class PrenotazioneServlet extends HttpServlet {
             response.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
             request.getRequestDispatcher("sessioneJSON.jsp").forward(request, response);
         }
-        
-        
-        
+         */
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -75,4 +116,14 @@ public class PrenotazioneServlet extends HttpServlet {
         return "Short description";
     }// </editor-fold>
 
+    private void pubblicaLista(HttpServletRequest request, HttpServletResponse response, List<SessioneDonazione> sessioni) throws ServletException, IOException {
+        request.setAttribute("listaSessioni", sessioni);
+        request.getRequestDispatcher("nuova-prenotazione.jsp").forward(request, response);
+    }
+
+    private void pubblicaErrore(HttpServletRequest request, HttpServletResponse response, String error) throws ServletException, IOException {
+        request.setAttribute("errorMessage", error);
+        request.setAttribute("link", "login.jsp");
+        request.getRequestDispatcher("error.jsp").forward(request, response);
+    }
 }
